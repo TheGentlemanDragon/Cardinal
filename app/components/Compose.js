081bfa5a2facf17073a0ac5e7ee64dd0f2110b0a
Nando Pena <fernando.pena@rackspace.com>
2018-04-19 02:57:19 -0500

    Add Name, Type, and Content Type properties

--- app/components/Compose.js
+++ app/components/Compose.js
@@ -1,20 +1,9 @@
 import { h } from 'hyperapp'
+import { Properties } from '../modules/constants'
 import ComposeElement from './ComposeElement'
 import './Compose.styl'
 
-const properties = [
-  {
-    name: 'name',
-    type: 'text',
-  },
-  {
-    name: 'type',
-    type: 'options',
-    options: ['Label', 'Image'],
-  },
-]
-
-export default () => ({ elements, selectedIndex }, { addElement }) => (
+export default () => ({ element, elements }, { addElement }) => (
   <div key="compose" class="compose-tab" container="column #top @stretch">
     {/* Elements Section */}
     <div class="compose-title" container="row #spread @center">
@@ -26,7 +15,7 @@ export default () => ({ elements, selectedIndex }, { addElement }) => (
     </div>
 
     {/* Elements List */}
-    <div class="compose-elements" container="column #top @stretch">
+    <div class="compose-items" container="column #top @stretch">
       {elements.length &&
         elements.map((item, index) => (
           <ComposeElement item={item} index={index} />
@@ -40,18 +29,45 @@ export default () => ({ elements, selectedIndex }, { addElement }) => (
     </div>
 
     {/* Properties List */}
-    <div class="compose-elements" container="column #top @stretch">
-      {selectedIndex > -1 &&
-        properties.map(prop => (
-          <div class="properties-item" container="row #spread @center">
-            <span flex>{prop.name}</span>
-            <input
-              type="text"
-              value={elements[selectedIndex][prop.name]}
-              flex
-            />
-          </div>
-        ))}
-    </div>
+    {element && (
+      <div class="compose-items" container="column #top @stretch">
+        {/* Element Name */}
+        <div class="property-input" container="row #spread @center">
+          <span>name</span>
+          <input type="text" value={element.name} />
+        </div>
+
+        {/* Element Type */}
+        <div class="property-input" container="row #spread @center">
+          <span>type</span>
+          <select>
+            {['Text', 'Image'].map(opt => (
+              <option selected={opt === element.type}>{opt}</option>
+            ))}
+          </select>
+        </div>
+
+        {/* Content Type */}
+        <div class="property-switch" container="row #left @center">
+          <span>content</span>
+
+          <input
+            type="radio"
+            name="contentType"
+            id="static"
+            checked={element.contentType}
+          />
+          <label for="static">static</label>
+
+          <input
+            type="radio"
+            name="contentType"
+            id="dynamic"
+            checked={element.contentType}
+          />
+          <label for="dynamic">dynamic</label>
+        </div>
+      </div>
+    )}
   </div>
 )

ab8860f30b86614c50a45fc918280bd8541bcf89
Nando Pena <fernando.pena@rackspace.com>
2018-04-25 13:55:48 -0500

    Begin Drop event logic

--- app/components/FileManager.js
+++ app/components/FileManager.js
@@ -4,8 +4,25 @@ import './FileManager.styl'
 const FileManager = () => (state, { fm }) => (
   Object.assign(fm, state.fm),
   fm.visible && (
-    <div class="file-manager-wrapper" onclick={() => fm.hide()}>
-      <div class="file-manager">POP</div>
+    <div
+      class="file-manager-wrapper"
+      onclick={event => event.currentTarget === event.srcElement && fm.hide()}
+    >
+      <div class="file-manager" container="column #top @stretch">
+        <div flex>Files:</div>
+        <div
+          class="file-manager-drop"
+          container="row #center @center"
+          flex
+          ondrop={event => {
+            console.log(event.dataTransfer.files[0])
+            event.preventDefault()
+          }}
+          ondragover={event => event.preventDefault()}
+        >
+          Drop Files Here
+        </div>
+      </div>
     </div>
   )
 )

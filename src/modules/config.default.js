365aeedc7617ceff827d3900511774453b4fd361
Nando Pena <fernando.pena@rackspace.com>
2018-09-11 01:21:17 -0500

    Add App, Games list, and Templates list

---
+++ src/modules/config.default.js
@@ -0,0 +1,9 @@
+// fill in values and rename file to 'config.js'
+export const options = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
+}

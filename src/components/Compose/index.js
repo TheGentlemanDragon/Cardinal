f1b1532f9ff40baea14408397f95bd34a09bdbd7
Nando Pena <fernando.pena@rackspace.com>
2019-01-08 01:27:26 -0600

    Properties and Style updateElement

--- src/components/Compose/index.js
+++ src/components/Compose/index.js
@@ -8,23 +8,18 @@ import Style from './Style'
 import { differ } from '../../modules/utils'
 
 class Compose extends Component {
-  origElements = null
-
-  // TODO: Update Compose state
-  updateState = partial => {}
-
   render() {
     const { elements, templatePage } = this.props
     const { elementIndex, preview, scale } = templatePage
     const element = elements[elementIndex] || {}
-    const modified = differ(this.props.template.elements, elements)
+    const modified = differ(templatePage.prevElements, elements)
 
     return (
       <>
         <Elements items={elements} index={elementIndex} modified={modified} />
-        <Properties element={element} onUpdate={this.updateState} />
-        <Style element={element} onUpdate={this.updateState} />
-        <Preview preview={preview} scale={scale} onUpdate={this.updateState} />
+        <Properties element={element} />
+        <Style element={element} />
+        <Preview preview={preview} scale={scale} />
       </>
     )
   }

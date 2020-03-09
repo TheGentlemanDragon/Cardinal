0e280180c5708f1d7b48b5410350e65c0234d2a7
Nando Pena <email@nandopena.dev>
2020-03-09 01:34:17 -0500

    Add home page and ItemList

--- src/components/app.js
+++ src/components/app.js
@@ -1,28 +1,22 @@
 import { h } from 'preact'
 import { Router } from 'preact-router'
 
-import Header from './header'
-
 // Code-splitting is automated for routes
 import Home from '../routes/home'
-import Profile from '../routes/profile'
+// import Profile from '../routes/profile'
+
+import s from './app.css'
 
 export default function App() {
-  /** Gets fired when the route changes.
-   *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
-   *	@param {string} event.url	The newly routed URL
-   */
-  //   handleRoute = e => {
-  //     this.currentUrl = e.url
-  //   }
-
   return (
     <div id="app">
-      <Header />
+      {/* App Title */}
+      <h1 class={s.title}>Cardinal</h1>
+
       <Router>
         <Home path="/" />
-        <Profile path="/profile/" user="me" />
-        <Profile path="/profile/:user" />
+        {/* <Profile path="/profile/" user="me" />
+        <Profile path="/profile/:user" /> */}
       </Router>
     </div>
   )

98dfd8282a07f6562bd056997c3e218b0f6f1849
Nando Pena <email@nandopena.dev>
2020-03-26 03:53:09 -0500

    Rename ItemList to FlixList; replace with GameList on home

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,16 +1,14 @@
 import { h } from 'preact'
-import ItemList from '../../components/ItemList'
-import GameItem from '../../components/GameItem'
-// import style from './style.css'
+import useGames from 'hooks/useGames'
+
+import GameList from 'components/GameList'
 
 function Home() {
+  const games = useGames()
+
   return (
     <>
-      <ItemList
-        title="Games"
-        items={['game1', 'game2', 'game3', 'game4', 'game5', 'game6']}
-        ItemComponent={GameItem}
-      />
+      <GameList title="Games" items={games} />
     </>
   )
 }

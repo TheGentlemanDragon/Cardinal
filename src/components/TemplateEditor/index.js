7125e676ead5b5d49e74e52161f1f34552026a9c
Nando Pena <email@nandopena.dev>
2020-07-08 00:49:00 -0500

    Generalize context creation

--- src/components/TemplateEditor/index.js
+++ src/components/TemplateEditor/index.js
@@ -1,9 +1,9 @@
 import { h } from 'preact'
 
-import s from './style.css'
 import EditorCard from 'components/EditorCard'
 import ScaleSlider from 'components/ScaleSlider'
 import { withEditorContext } from 'contexts/EditorContext'
+import s from './style.css'
 
 function TemplateEditor() {
   return (
--- src/contexts/EditorContext.js
+++ src/contexts/EditorContext.js
@@ -1,43 +1,9 @@
-import { createContext } from 'preact'
-import { useState } from 'preact/hooks'
+import { useContextEx } from 'lib/context'
 
-const contextKey = 'EditorContext'
-
-let initial = JSON.parse(localStorage.getItem(contextKey))
-
-if (!initial) {
-  initial = {
-    scale: 2.0,
-  }
+const defaults = {
+  scale: 2.0,
 }
 
-const EditorContext = createContext({
-  scale: 0,
-  setScale: () => {},
-})
+const [useEditorContext, withEditorContext] = useContextEx(defaults, 'Editor')
 
-export default EditorContext
-
-export function withEditorContext(Component, persist) {
-  return function(props) {
-    const [scale, setScale] = useState(initial.scale)
-
-    const value = {
-      scale,
-      setScale: scale => {
-        if (persist) {
-          const data = JSON.parse(localStorage.getItem(contextKey)) || {}
-          localStorage.setItem(contextKey, JSON.stringify({ ...data, scale }))
-        }
-
-        setScale(scale)
-      },
-    }
-
-    return (
-      <EditorContext.Provider value={value}>
-        <Component {...props} />
-      </EditorContext.Provider>
-    )
-  }
-}
+export { useEditorContext, withEditorContext }

7408625288595a2fa6f9925b0a6f3c745972dfee
Nando Pena <email@nandopena.dev>
2020-07-18 16:14:36 -0500

    Remove font icons

--- src/components/app.js
+++ src/components/app.js
@@ -5,7 +5,6 @@ import { Router } from 'preact-router'
 import { HomePage, TemplatesPage, EditorPage } from 'components/Pages'
 
 import g from './global.css'
-import './fonts.css'
 
 export default function App() {
   return (

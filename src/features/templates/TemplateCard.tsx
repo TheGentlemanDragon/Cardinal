45c3cd73078f3bfdba0ab6f9e5bedd69462403c5
Nando Pena <email@nandopena.dev>
2025-03-05 00:50:43 -0600

    Add Project page with Template list

---
+++ src/features/templates/TemplateCard.tsx
@@ -0,0 +1,71 @@
+import { Template } from "../../lib/templates";
+import { cls, MENU_BUTTON_CLS, CARD_CLS } from "../../lib/styles";
+import { timeSince } from "../../lib/utils";
+import { MenuIcon } from "../icons/MenuIcon";
+import { TrashIcon } from "../icons/TrashIcon";
+
+type Props = {
+  template: Template;
+};
+
+export const TemplateCard = ({ template }: Props) => {
+  return (
+    <article
+      class={
+        CARD_CLS +
+        ` w-40 h-64 aspect-[0.712] 
+        border-[#1565c0] border-2 border-dashed`
+      }
+    >
+      {/* Card image */}
+      <figure>
+        <img
+          src={`https://picsum.photos/seed/${template.id}/160/155`}
+          alt={`${template.name}`}
+        />
+      </figure>
+
+      {/* Card actions menu */}
+      <div
+        class="
+          dropdown absolute hidden
+          top-2 right-2
+          group-hover:block group-focus-within:block"
+      >
+        <div
+          class={cls(MENU_BUTTON_CLS, "btn-xs btn-square rounded-lg")}
+          role="button"
+          tabindex={0}
+        >
+          <MenuIcon />
+        </div>
+        <ul
+          class="
+            dropdown-content menu
+            bg-base-200 rounded-lg
+            z-[1] w-32 p-2
+            shadow-md shadow-blue-950/80"
+          data-theme="dark"
+          tabindex={0}
+        >
+          <li>
+            <a class="text-red-400/90 rounded-md pl-2">
+              <TrashIcon cls="h-4 w-5" />
+              <span>Delete</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      {/* Card body */}
+      <div class="card-body gap-0">
+        <h2 class="card-title text-base">
+          <a class="link" href={`/projects/${template.id}`}>
+            {template.name}
+          </a>
+        </h2>
+        <p class="text-xs">{timeSince(template.created)}</p>
+      </div>
+    </article>
+  );
+};
---
+++ src/lib/templates.ts
@@ -0,0 +1,43 @@
+import { useQuery } from "@tanstack/react-query";
+import { PbList, ignore404, pb, queryClient } from "./db";
+import { userSignal } from "./user";
+
+const TEMPLATES = pb.collection("cardinal_templates");
+const queryKey = ["templates"];
+
+export type Template = {
+  collectionId: string;
+  collectionName: string;
+  created: Date;
+  id: string;
+  name: string;
+  owner: string;
+  project: string;
+  updated: Date;
+};
+
+export const create = async (name: string, projectId: string) => {
+  await TEMPLATES.create({
+    name,
+    owner: userSignal.value?.id,
+    project: projectId,
+  });
+  queryClient.invalidateQueries({ queryKey });
+};
+
+export const useTemplatesList = (projectId?: string) =>
+  useQuery<PbList<Template>>({
+    enabled: projectId != undefined,
+    queryFn: ignore404(() =>
+      TEMPLATES.getList(1, 20, { filter: `project.id="${projectId}"` })
+    ),
+    queryKey,
+    select: (data) => ({
+      ...data,
+      items: data.items.map((item) => ({
+        ...item,
+        created: new Date(item.created),
+        updated: new Date(item.updated),
+      })),
+    }),
+  });

e9ae54f37714beff82d1f28cb5a25cedf9c04445
Nando Pena <email@nandopena.dev>
2025-03-05 00:50:43 -0600

    Add Project page with Template list

---
+++ src/features/templates/CreateTemplateModal.tsx
@@ -0,0 +1,75 @@
+import { useRoute } from "preact-iso";
+import { useRef, useState } from "preact/hooks";
+import { PlusIcon } from "../icons/PlusIcon";
+import { create, useTemplatesList } from "../../lib/templates";
+import { Modal } from "../Modal";
+
+export const CreateTemplateModal = () => {
+  const route = useRoute();
+  const projectId = route.params.id;
+
+  const { refetch } = useTemplatesList(projectId);
+  const dialog = useRef<HTMLDialogElement>(null);
+  const [name, setName] = useState("");
+
+  const createTemplate = () => {
+    create(name, projectId);
+    refetch();
+    dialog.current?.close();
+  };
+
+  return (
+    <>
+      <div className="tooltip" data-tip="Create new project">
+        <button
+          class="btn btn-sm btn-neutral btn-circle"
+          onClick={() => dialog.current?.showModal()}
+        >
+          <PlusIcon />
+        </button>
+      </div>
+
+      <dialog class="modal" id="createProjectModal" ref={dialog}>
+        <Modal>
+          <Modal.Title>Create a new template</Modal.Title>
+
+          <Modal.Content>
+            <p>
+              A templates is a card blueprint consisting of static or dynamic
+              text and/or images. You can change these values later.
+            </p>
+
+            <label class="form-control w-full max-w-xs">
+              <div class="label">
+                <span class="label-text">Template Name</span>
+              </div>
+              <input
+                type="text"
+                placeholder="Type here"
+                class="input input-bordered w-full max-w-xs"
+                onInput={(event) => setName(event.currentTarget.value)}
+              />
+            </label>
+          </Modal.Content>
+
+          <Modal.Actions>
+            <button
+              class="btn btn-ghost"
+              onClick={() => dialog.current?.close()}
+            >
+              Cancel
+            </button>
+
+            <button
+              class="btn btn-primary"
+              disabled={name.length === 0}
+              onClick={createTemplate}
+            >
+              Confirm
+            </button>
+          </Modal.Actions>
+        </Modal>
+      </dialog>
+    </>
+  );
+};

dc3fc7321200e046c467c67d8fdc57f77c8060cf
Nando Pena <email@nandopena.dev>
2025-10-11 13:54:44 -0500

    Sort styles

--- src/lib/styles.ts
+++ src/lib/styles.ts
@@ -1,10 +1,4 @@
-export const INPUT_ICON_CLS = `_
-  w-8 h-8 absolute top-1/2 right-3
-  transform -translate-y-1/2
-  text-gray-400 pointer-events-none  
-`;
-
-export const MENU_BUTTON_CLS = "btn btn-neutral hover:bg-gray-700";
+export const cls = (...args: string[]) => args.filter((item) => item).join(" ");
 
 export const CARD_CLS = `_
   relative group
@@ -15,13 +9,19 @@ export const CARD_CLS = `_
   cursor-pointer
 `;
 
-export const TEXT_SHADOW_CLS = "[text-shadow:2px_2px_4px_rgb(0_0_0/30%)]";
-
-export const TITLE_CLS = "text-2xl font-semibold";
-
 export const HIDE_ARROWS =
   "[appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none";
 
-export const cls = (...args: string[]) => args.filter((item) => item).join(" ");
+export const INPUT_ICON_CLS = `_
+  w-8 h-8 absolute top-1/2 right-3
+  transform -translate-y-1/2
+  text-gray-400 pointer-events-none  
+`;
 
 export const MENU_CLS = "bg-base-200 rounded-box shadow-md menu w-full";
+
+export const MENU_BUTTON_CLS = "btn btn-neutral hover:bg-gray-700";
+
+export const TEXT_SHADOW_CLS = "[text-shadow:2px_2px_4px_rgb(0_0_0/30%)]";
+
+export const TITLE_CLS = "text-2xl font-semibold";

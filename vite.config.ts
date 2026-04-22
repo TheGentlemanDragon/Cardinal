ecb4fb003a2720dbad1409e021d5d88c85214160
Nando Pena <email@nandopena.dev>
2026-04-21 23:53:02 -0500

    Update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,40 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import path from "path";
 
+/* Note: Daisy UI aliases are to address error
+> [plugin @tailwindcss/vite:generate:build] /home/nando/Code/cardinal/src/styles.css
+> TypeError: Unknown file extension ".css" for /home/nando/Code/cardinal/node_modules/daisyui/daisyui.css
+> TypeError [ERR_UNKNOWN_FILE_EXTENSION]: Unknown file extension ".css" for /home/nando/Code/cardinal/node_modules/daisyui/daisyui.css
+*/
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), preact()],
+  plugins: [preact(), tailwindcss()],
   resolve: {
-    alias: {
-      $assets: path.resolve(__dirname, "./src/assets"),
-      $components: path.resolve(__dirname, "./src/components"),
-      $lib: path.resolve(__dirname, "./src/lib"),
-    },
+    alias: [
+      {
+        find: /^daisyui$/,
+        replacement: path.resolve(__dirname, "./node_modules/daisyui/index.js"),
+      },
+      {
+        find: /^daisyui\/theme$/,
+        replacement: path.resolve(
+          __dirname,
+          "./node_modules/daisyui/theme/index.js",
+        ),
+      },
+      {
+        find: "$assets",
+        replacement: path.resolve(__dirname, "./src/assets"),
+      },
+      {
+        find: "$components",
+        replacement: path.resolve(__dirname, "./src/components"),
+      },
+      {
+        find: "$lib",
+        replacement: path.resolve(__dirname, "./src/lib"),
+      },
+    ],
   },
 });
